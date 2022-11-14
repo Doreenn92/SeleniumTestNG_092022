879722231ff2c2b2aed9567b481a396535d4bdc5
Dinh Nguyen (Doreen) <nguyenduydinh2110@gmail.com>
2022-11-14 21:03:00 +0700

    TestNG 14 Nov 2022

---
+++ src/test/java/doreen/com/TestRunXML/RunTest03.java
@@ -0,0 +1,20 @@
+package doreen.com.TestRunXML;
+
+import org.testng.annotations.Test;
+
+public class RunTest03 {
+    @Test
+    void test03(){
+        System.out.println("Run test03");
+    }
+
+    @Test
+    void test04(){
+        System.out.println("Run test04");
+    }
+
+    @Test
+    void test05(){
+        System.out.println("Run test05");
+    }
+}
